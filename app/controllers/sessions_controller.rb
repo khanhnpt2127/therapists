452880de6a22c5eacb602aeb2fa54d8303e8a662
Alecks Johansson <alecksjohansson@Aleckss-MacBook-Pro.local>
2016-08-15 21:12:26 +0700

    Fixing No Data bugs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if @user = User.find_by(email: params[:email])
-      if @user.authenticate(params[:password])
-        session[:user_id] = @user.id
+    if @host = Host.find_by(email: params[:email])
+      if @host.authenticate(params[:password])
+        session[:host_id] = @host.id
         redirect_to root_path
       else
         redirect_to new_session_path, notice: "Password is wrong"
       end
     else
     redirect_to new_session_path, notice: "Email is not exist"
-    end  
+    end
   end
-  
+
   def destroy
     session[:user_id] = nil
     redirect_to root_path
