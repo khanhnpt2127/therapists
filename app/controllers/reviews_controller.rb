ad8ade8b29a26e2bbb14b8f726c58de28899eee9
Alecks Johansson <alecksjohansson@Aleckss-MacBook-Pro.local>
2016-08-15 20:34:03 +0700

    Added Profile Comment

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,8 +9,11 @@ class ReviewsController < ApplicationController
       @review = current_host.reviews.new review_params
       @review.reviewable = current_host
       @review.host_id = current_host if current_host
+
     end
-        @review.host_id = params[:host_id]
+      @review.user_id = params[:user_id]
+      @review.host_id = params[:host_id]
+
     @review.save!
     redirect_to @reviewable, notice: "Your comment was posted"
   end
