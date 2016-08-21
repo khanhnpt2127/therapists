ade2528b32fca83c2dd55fa25f130394ea08e7de
nhn8957 <nhiemngohoang@yahoo.com>
2016-08-22 01:52:21 +0700

    UX almost done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,15 @@ class UsersController < ApplicationController
   end
 
   def edit
+      @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update user_params
+      redirect_to user_path(:id => current_user.id), notice: "Information updated"
+    else render 'edit' 
+    end  
   end
 
   private
